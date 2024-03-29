1653d44784442095c2ea6d88a2a68f6dd3aba680
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-03-29 20:54:00 +0800

    📂Modified windows.html and change its header title.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Crabby',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Crabby',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Crabby Logo',
           src: 'img/logo.svg',
         },
         items: [
