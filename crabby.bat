d7e1260b5174aade322217b3f27817ea4b583daa
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-03-29 19:15:46 +0800

    📂Added download page for windows crabby && crabby.bat

---
+++ crabby.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Welcome to the Crabby: The new programming language!
+echo - By StarloExoliz and the Team!
+
+echo You can check https://github.com/Kazooki123/crabby for the repo
+pause
