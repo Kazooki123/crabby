353c684d77876216c103c00cbd2e389281b8d10b
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-10-16 13:45:20 +0800

    BIG CHANGE: Switched from Zig to C, added Makefile

--- crabby.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Welcome to the Crabby: The new programming language!
-echo - By StarloExoliz and the Team!
-
-echo You can check https://github.com/Kazooki123/crabby for the repo
-pause
--- lib/func/Function.zig
+++
@@ -1,2 +0,0 @@
-// function syntax for crabby "fun" syntax
-
